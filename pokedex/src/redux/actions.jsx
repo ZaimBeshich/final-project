4566af1016cf3440d146b58db0a988916395d775
Zaim <wtfgagarin@gmail.com>
2021-03-17 04:35:49 +0300

    feat: add actions

---
+++ pokedex/src/redux/actions.jsx
@@ -0,0 +1,21 @@
+import JsonService from "../service/service";
+
+const service = new JsonService();
+
+export const getPokemons = count => async (dispatch, getState) => {
+    const pokemons = await service.getAllPokemons(count);
+    dispatch({
+        type: 'GET_POKEMONS',
+        payload: pokemons
+    })
+}
+
+export const catchPokemon = id => ({
+    type: 'CATCH_POKEMON',
+    payload: id
+})
+
+export const selectPokemon = desc => ({
+    type: 'SELECT_POKEMON',
+    payload: desc
+})
