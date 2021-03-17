ace5da62269c0a339ba7b62d657dec02d60b195c
Zaim <wtfgagarin@gmail.com>
2021-03-17 04:36:03 +0300

    feat: update App

--- pokedex/src/components/App/App.js
+++ pokedex/src/components/App/App.js
@@ -1,14 +1,38 @@
-import NavBar from '../Nav/Nav';
-import Main from '../Main/Main';
+import React, { useEffect } from "react";
+import { Redirect, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Main from "../Main/MainContainer";
+import Collection from "../Collection/CollectionContainer"
+import Navigation from "../Nav/Nav";
+import PokemonPage from "../PokemonPage/PokemonContainer";
+import { getPokemons } from "../../redux/actions";
+import store from '../../redux/reducer';
 
+const App = () => {
+  useEffect(() => {
+    store.dispatch(getPokemons(0));
+  }, []);
 
-function App () {
   return (
-    <div>
-      <NavBar/>
-      <Main/>
-    </div>
-  )
-};
+    <Router>
+      <div className='111'>
+        <Navigation />
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route path="/home" exact render={
+            () => <Main />
+          } />
+
+          <Route path="/collection" render={
+            () => <Collection />
+          } />
+
+          <Route path="/PokemonPage/:name" render={
+            () => <PokemonPage />
+          } />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
