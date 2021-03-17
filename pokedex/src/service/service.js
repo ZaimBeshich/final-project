80a156763d4793ada94b6dbc0f410e499770d6cd
Zaim <wtfgagarin@gmail.com>
2021-03-17 04:35:06 +0300

    feat: add services

---
+++ pokedex/src/service/service.js
@@ -0,0 +1,26 @@
+export default class JsonService {
+  apiBase = "http://localhost:3000";
+
+  async getResource(url='') {
+    const result = await fetch(`${this.apiBase}${url}`);
+
+    if(!result.ok) {
+      throw new Error (`Couldn't fetch ${url}, received ${result.status}`)
+    }
+
+    let sas = await result.json();
+    return sas;
+  }
+
+  async getAllPokemons(count = 0) {
+    const pokemons = await this.getResource(`/pokemons/`);
+
+    const result = pokemons.slice(count, count + 14);
+    return result;
+  }
+
+  async getPokemon(id) {
+    const pokemon = await this.getResource(`/pokemons/${id}`);
+    return pokemon;
+  }
+}
